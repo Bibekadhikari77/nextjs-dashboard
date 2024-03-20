5eab7ceadd8ef589e3ecfbde510155463c809318
bibek <adhikaribibek84@gmail.com>
2024-03-20 17:09:03 +0545

    added fetchInvoicePages inside invoices page.tsx file

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,8 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
- 
+import { fetchInvoicesPages } from '@/app/lib/data';
+
 export default async function Page({
   searchParams,
 }: {
@@ -15,7 +16,12 @@ export default async function Page({
   };
 }) {
   const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;  return (
+  const currentPage = Number(searchParams?.page) || 1;  
+
+  const totalPages = await fetchInvoicesPages(query);
+
+
+  return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
