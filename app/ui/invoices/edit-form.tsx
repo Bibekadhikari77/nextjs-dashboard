851d14cd42d4a70c08c5da3b1bc51fbff0877ad9
bibek <adhikaribibek84@gmail.com>
2024-03-31 15:11:02 +0545

    chapter 14 completed

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -11,7 +11,6 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
 
-
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -19,9 +18,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-
-  return ( 
+  //const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+  
+  return (
     <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
